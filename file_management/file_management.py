0ae15312feecd9e7429d7be80a7c11ff1f4b9592
pentrican10 <paige.eee1014@gmail.com>
2023-07-20 13:19:52 -0700

    Update file_management.py

--- file_management/file_management.py
+++ file_management/file_management.py
@@ -1,23 +1,22 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 import os
 app = Flask(__name__)
-
 app.secret_key='secret'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        session['project_dir'] = request.form.get('project_dir','')
+        session['project_dir'] = request.form.get('project_dir') #None
         #session['project_dir']=project_dir
 
-    project_dir = session.get('project_dir','')
+    project_dir = session.get('project_dir') #None
     data_dir = os.path.join(project_dir, 'miniflask','file_management','comments')
     session['data_dir']= data_dir
     return render_template('index.html', project_dir=project_dir, data_dir=data_dir)
 
 @app.route('/save_project_dir', methods=['POST'])
 def save_project_dir():
-    session['project_dir']= request.form.get('project_dir','')
+    session['project_dir']= request.form.get('project_dir') #None
     return redirect(url_for('index'))
 
 def read_star_file(star_id):
@@ -35,7 +34,7 @@ def read_star_file(star_id):
 def save_star_id():
     star_id = request.form.get('star_id')
     star_file_content = read_star_file(star_id)
-    data_dir = session.get('data_dir','')
+    data_dir = session.get('data_dir') #None
     return render_template('index.html', data_dir=data_dir, star_id=star_id, star_file_content=star_file_content)
 
 if __name__ == '__main__':
