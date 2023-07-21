d47844d5eca0bd22f05a6ac9a84d6ee3d9efa9d6
pentrican10 <paige.eee1014@gmail.com>
2023-07-21 11:02:17 -0700

    change/update names

--- render_plot/render_plot.py
+++ render_plot/render_plot.py
@@ -17,7 +17,7 @@ def home():
     star_id=None
     project_dir = None
     data_dir = None
-    return render_template('index1.html', project_dir=project_dir, star_id=star_id, plot_div=plot_div, data_dir=data_dir)
+    return render_template('index.html', project_dir=project_dir, star_id=star_id, plot_div=plot_div, data_dir=data_dir)
 
 
 @app.route('/save_project_dir', methods=['POST'])
@@ -27,7 +27,7 @@ def save_project_dir():
     data_dir=os.path.join(project_dir, 'kepler_lightcurves_for_paige')
     session['data_dir']=data_dir
     #return redirect(url_for('index'))
-    return render_template('index1.html', project_dir=project_dir)
+    return render_template('index.html', project_dir=project_dir)
 
 @app.route('/save_star_id', methods=['POST'])
 def save_star_id():
@@ -36,7 +36,7 @@ def save_star_id():
 
     project_dir = session.get('project_dir','')
     data_dir = session.get('data_dir') 
-    return render_template('index1.html', star_id=star_id, project_dir=project_dir)
+    return render_template('index.html', star_id=star_id, project_dir=project_dir)
 
 @app.route('/plot', methods=['POST'])
 def plot():
@@ -53,7 +53,7 @@ def plot():
                  title="Kepler Detrended Light Curve")
     plot_div = fig.to_html(full_html=False, include_plotlyjs='cdn')
 
-    return render_template('index1.html', plot_div=plot_div, project_dir=project_dir, star_id =star_id)
+    return render_template('index.html', plot_div=plot_div, project_dir=project_dir, star_id =star_id)
     #return render_template('index.html',plot_div=plot_div, project_dir=project_dir,star_id=star_id)
 
 
